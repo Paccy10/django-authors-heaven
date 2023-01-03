c174fe049b44cc6ba3d1aeae6e08fe06a6964094
Pacifique Clement Ndayisenga <32579116+Paccy10@users.noreply.github.com>
2023-01-03 12:16:10 +0200

    feature: get articles

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -2,15 +2,47 @@ from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Article
-from .serializers import NewArticleSerializer
+from .serializers import NewArticleSerializer, ArticleDisplaySerializer
+from .helpers.utils import filter_queryset
 
 
-class ArticlesView(mixins.CreateModelMixin, generics.GenericAPIView):
+class ArticlesView(
+    mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericAPIView
+):
     """Articles view"""
 
-    queryset = Article.objects.all()
     serializer_class = NewArticleSerializer
     permission_classes = [IsAuthenticated]
+    ordering = ["-pkid"]
+    search_fields = [
+        "title",
+        "body",
+        "tags__name",
+        "author__first_name",
+        "author__last_name",
+        "author__middle_name",
+    ]
+
+    def get_queryset(self):
+        return filter_queryset(Article.objects.all(), self.request.user)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        self.serializer_class = ArticleDisplaySerializer
+        return self.list(request, *args, **kwargs)
+
+
+class ArticleView(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    """Artcicle view"""
+
+    serializer_class = ArticleDisplaySerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "slug"
+
+    def get_queryset(self):
+        return filter_queryset(Article.objects.all(), self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,16 @@
 from django.urls import reverse
+from apps.articles.models import Article
 
 
 def get_dynamic_url(model, url_name):
     instance = model.objects.first()
     url = reverse(url_name, args=[instance.id])
+
+    return url
+
+
+def get_article_dynamic_url():
+    article = Article.objects.first()
+    url = reverse("single-article", args=[article.slug])
 
     return url
